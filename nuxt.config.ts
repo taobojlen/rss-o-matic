56c2124ed5d3bfe6aa58a9cc9b30db8f3e0b681b
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2026-02-24 10:51:20 +0000

    fix posthog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ['@nuxthub/core'],
+  modules: ['@nuxthub/core', '@posthog/nuxt'],
   hub: {
     db: {
       dialect: 'sqlite',
@@ -16,11 +16,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     openrouterApiKey: '',
     openrouterModel: '',
-    public: {
-      posthogPublicKey: 'phc_CkAp2xq3iYAT1KT7v4TVAmDSR6zjQ0JoBnDpCL4AMS9',
-      posthogHost: 'https://eu.i.posthog.com',
-      posthogDefaults: '2026-01-30'
-    }
   },
   app: {
     head: {
@@ -34,5 +29,31 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   nitro: {
     preset: 'cloudflare_module',
+    rollupConfig: {
+      output: {
+        sourcemapExcludeSources: false
+      }
+    }
   },
+  sourcemap: {
+    client: 'hidden',
+    server: true,
+  },
+  posthogConfig: {
+    publicKey: 'phc_CkAp2xq3iYAT1KT7v4TVAmDSR6zjQ0JoBnDpCL4AMS9', // Find it in project settings https://app.posthog.com/settings/project
+    host: 'https://eu.i.posthog.com', // Optional: defaults to https://us.i.posthog.com. Use https://eu.i.posthog.com for EU region
+    clientConfig: {
+      capture_exceptions: true, // Enables automatic exception capture on the client side (Vue)
+      defaults: '2026-01-30',
+    },
+    serverConfig: {
+      enableExceptionAutocapture: true, // Enables automatic exception capture on the server side (Nitro)
+    },
+    sourcemaps: {
+      enabled: true,
+      project: '131385', // Your project ID from PostHog settings https://app.posthog.com/settings/environment#variables
+      personalApiKey: process.env.POSTHOG_PERSONAL_API_KEY ?? '', // Your personal API key from PostHog settings https://app.posthog.com/settings/user-api-keys (requires organization:read and error_tracking:write scopes)
+      releaseName: 'rss-o-matic' // Optional: defaults to git repository name
+    },
+  }
 })
